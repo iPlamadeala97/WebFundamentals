c772aabf96b8ac8852d653f653b4f999c2f2f371
iPlamadeala97 <82528152+iPlamadeala97@users.noreply.github.com>
2021-04-26 21:58:10 -0400

    Add files via upload

---
+++ LoopsChallenge.js
@@ -0,0 +1,24 @@
+//Print Odds 1-20
+for (var i=0; i<21; i++) {
+    if (i % 2 != 0 ) console.log(i);
+}
+
+//Print The Sequence
+for (var i=4; i>-3.6; i-=1.5) {
+    console.log(i);
+}
+
+//Sigma
+var sum=0
+for (var i=0; i<101; i++) {
+    sum = sum + i;
+}
+console.log(sum)
+
+//Factorial
+var factorial=1
+
+for (i=1; i<1e8; i++) {
+    factorial *= i;
+}
+console.log(i)
